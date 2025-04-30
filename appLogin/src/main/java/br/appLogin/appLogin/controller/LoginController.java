97afdd97856c3b26bc6a69ea33160e047d8c1843
Marco Antônio <marcosynky@gmail.com>
2025-04-30 13:11:42 -0300

    Commit

--- appLogin/src/main/java/br/appLogin/appLogin/controller/LoginController.java
+++ appLogin/src/main/java/br/appLogin/appLogin/controller/LoginController.java
@@ -2,45 +2,62 @@ package br.appLogin.appLogin.controller;
 
 import br.appLogin.appLogin.model.Usuario;
 import br.appLogin.appLogin.repository.UsuarioRepository;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class LoginController {
+
     @Autowired
     private UsuarioRepository ur;
 
     @GetMapping("/login")
     public String login() {
-        return "login";
+        return "login"; // Página de login
     }
+
+    @GetMapping("/home")
+    public String home() {
+        return "home"; // Retorna a página home.html
+    }
+
+
+    @PostMapping("/logar")
+    public String loginUsuario(Usuario usuario, HttpServletResponse response) {
+        Usuario user = ur.login(usuario.getEmail(), usuario.getSenha());
+        if (user != null) {
+            return "redirect:/home"; // Redireciona para a página home se o login for bem-sucedido
+        } else {
+            return "redirect:/login"; // Redireciona para a página de login em caso de falha no login
+        }
+    }
+
+
+
 
     @GetMapping("/cadastroUsuario")
     public String cadastro(Model model) {
         model.addAttribute("usuario", new Usuario()); // Cria um novo objeto 'Usuario' e passa para o template
-        return "cadastro"; // Nome do template
+        return "cadastro"; // Página de cadastro
     }
 
-    @RequestMapping(value = "/cadastroUsuario",method = RequestMethod.POST )
-     public String cadatroUsuario(@Valid Usuario usuario, BindingResult result) {
-
+    @RequestMapping(value = "/cadastroUsuario", method = RequestMethod.POST)
+    public String cadastroUsuario(@Valid Usuario usuario, BindingResult result, Model model) {
+        // Verifica se houve erros de validação
         if (result.hasErrors()) {
-            return "redirect:/cadastroUsuario";
+            return "cadastro"; // Retorna para a página de cadastro em caso de erro
         }
 
+        // Salva o usuário no banco de dados
         ur.save(usuario);
-        return "redirect:/login";
-     }
-
+        return "redirect:/login"; // Redireciona para a página de login após o cadastro
+    }
 }
-
-
 
 
