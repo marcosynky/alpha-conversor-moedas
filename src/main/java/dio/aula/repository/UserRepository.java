9b9d1f2f8fe4c4bb48b52847e667291d80a127b3
Marco Antônio <165909014+marcosynky@users.noreply.github.com>
2025-05-03 04:54:36 -0300

    Delete src directory

--- src/main/java/dio/aula/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package dio.aula.repository;
-
-import dio.aula.model.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<Usuario, Integer> {
-    // Aqui você já tem todos os métodos básicos como save(), findById(), etc.
-}
