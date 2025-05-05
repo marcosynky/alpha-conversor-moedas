eae87c1980e24181d75c398e78c2fd0d5b82ff36
Marco Antônio <marcosynky@gmail.com>
2025-05-05 13:15:25 -0300

    Mensagem de commit

---
+++ projeto-conversor-de-moedas/src/main/java/com/example/demo/ProjetoConversorDeMoedasApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoConversorDeMoedasApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ProjetoConversorDeMoedasApplication.class, args);
+	}
+
+}
